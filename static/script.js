aa76fbe16a6c84b0f7208fda4561ef9bdcd17a94
evanleonille <133546962+evanleonille@users.noreply.github.com>
2023-10-16 00:12:20 +0800

    Add files via upload

---
+++ static/script.js
@@ -0,0 +1,23 @@
+// JavaScript to show/hide the dropdown content for all buttons with the class "dropdown-button"
+var buttons = document.querySelectorAll(".dropdown-button");
+
+buttons.forEach(function(button) {
+    button.addEventListener("click", function(event) {
+        event.stopPropagation();
+        var dropdown = button.nextElementSibling;
+        if (dropdown.style.display === "block") {
+            dropdown.style.display = "none";
+        } else {
+            dropdown.style.display = "block";
+        }
+    });
+});
+
+document.addEventListener("click", function(event) {
+    buttons.forEach(function(button) {
+        var dropdown = button.nextElementSibling;
+        if (event.target !== button) {
+            dropdown.style.display = "none";
+        }
+    });
+});
