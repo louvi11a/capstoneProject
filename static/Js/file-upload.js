6dcf388011e1e9687e301d802c6ec9d1f7391c14
evanleonille <evanleonille@gmail.com>
2023-11-25 10:38:44 +0800

    added file html , java and css

---
+++ static/Js/file-upload.js
@@ -0,0 +1,9 @@
+function openPopupForm() {
+    var popup = document.getElementById("popup-form");
+    popup.style.display = "block";
+}
+
+function closePopupForm() {
+    var popup = document.getElementById("popup-form");
+    popup.style.display = "none";
+}
