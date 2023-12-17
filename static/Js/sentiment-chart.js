c95dd6de760ca77a10944232054c09667c48dbfb
evanleonille <evanleonille@gmail.com>
2023-12-17 21:54:16 +0800

    fixed dashboard

---
+++ static/Js/sentiment-chart.js
@@ -0,0 +1,75 @@
+var orphans = {{ orphans|safe }};
+var sentiment_scores = {{ sentiment_scores|safe }};
+
+var ctxPie = document.getElementById('pieChart').getContext('2d');
+var pieChart = new Chart(ctxPie, {
+    type: 'pie',
+    data: {
+        labels: orphans,
+        datasets: [{
+            data: sentiment_scores,
+            // ... rest of your pie chart configuration
+        }]
+    },
+    // ... rest of your pie chart options
+    options: {
+        responsive: true,
+        plugins: {
+            title: {
+                display: true,
+                text: 'Sentiment Scores'
+            },
+            tooltip: {
+                callbacks: {
+                    label: function(tooltipItem) {
+                        return orphans[tooltipItem.dataIndex] + ': ' + sentiment_scores[tooltipItem.dataIndex].toFixed(3);
+                    }
+                }
+            }
+        }
+    }
+});
+
+var ctxBar = document.getElementById('barChart').getContext('2d');
+var barChart = new Chart(ctxBar, {
+    type: 'bar',
+    data: {
+        labels: orphans,
+        datasets: [{
+            data: sentiment_scores,
+            // ... rest of your bar chart configuration
+        }]
+    },
+    // ... rest of your bar chart options
+    options: {
+        responsive: true,
+        plugins: {
+            title: {
+                display: true,
+                text: 'Sentiment Scores per Orphan'
+            },
+            tooltip: {
+                callbacks: {
+                    label: function(tooltipItem) {
+                        return 'Sentiment Score: ' + tooltipItem.parsed.ytoFixed(3);
+                    }
+                }
+            }
+        },
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+});
+
+document.getElementById('switchChartPie').addEventListener('click', function() {
+    document.getElementById('pieChartContainer').style.display = 'none';
+    document.getElementById('barChartContainer').style.display = 'block';
+});
+
+document.getElementById('switchChartBar').addEventListener('click', function() {
+    document.getElementById('barChartContainer').style.display = 'none';
+    document.getElementById('pieChartContainer').style.display = 'block';
+});
